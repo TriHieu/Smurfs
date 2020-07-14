11661458a7c0c93025618f55d2bfa0a3c1852493
Hoàng Trương <hoangtruong181099@gmail.com>
2020-07-14 14:47:23 +0700

    filter history

--- App/js/history.js
+++ App/js/history.js
@@ -2,50 +2,53 @@ function dateTimeConvert(timestamp){
   let date = new Date(timestamp);
   return date;
 }
+
 async function drawTableTH() {
     let html = "";
-    let arr = [];
     let tbheader='';
     await fetch("/apicall/temphumi")
     .then((res)=> res.json())
     .then(dat => {
       for(let i = dat.length -1;i>0;i--){
         var e = dat[i];
-        if (e[0]==undefined)
-          continue;
+        if (e[0]==undefined) // check error
+          continue; //skip
         dtime =  dateTimeConvert(e.time_stamp).toString();
+        var ts =  dateTimeConvert(e.time_stamp);
+        var t = ts.getFullYear()+"-"+((ts.getMonth()+1)<10?'0' + (ts.getMonth()+1):ts.getMonth()+1)+"-"+ts.getDate();
         let tdstyle0 = "";
         let tdstyle1 = "";
-        console.log(e[0]);
-        (e[0].values[0]>26||e[0].values[0]<22)?tdstyle0 = "color: red":"";
-        (e[0].values[1]>75||e[0].values[1]<70)?tdstyle1 = "color: red":"";
-        tbheader = `<thead>
-                        <tr>
-                            <th>Time</th>
-                            <th>SensorID</th>
-                            <th>Temperature (C)</th>
-                            <th>Humidity (%)</th>
-                        </tr>
-                    </thead>
-                    <tbody id="tbody">
-                    </tbody>`
-        html += `<tr><td>`+dtime+`</td>
-                    <td>`+e[0].device_id+`</td>
-                    <td style = '`+tdstyle0+`'>`+e[0].values[0]+`</td>
-                    <td style = '`+tdstyle1+`'>`+e[0].values[1]+`</td>
-                </tr>`
+        if(t==document.getElementById("date-input").value || document.getElementById("date-input").value==""){
+            (e[0].values[0]>26||e[0].values[0]<22)?tdstyle0 = "color: red":"";
+            (e[0].values[1]>75||e[0].values[1]<70)?tdstyle1 = "color: red":"";
+          tbheader = `<thead>
+                          <tr>
+                              <th>Time</th>
+                              <th>SensorID</th>
+                              <th>Temperature (C)</th>
+                              <th>Humidity (%)</th>
+                          </tr>
+                      </thead>
+                      <tbody id="tbody">
+                      </tbody>`
+          html += `<tr><td>`+dtime+`</td>
+                      <td>`+e[0].device_id+`</td>
+                      <td style = '`+tdstyle0+`'>`+e[0].values[0]+`</td>
+                      <td style = '`+tdstyle1+`'>`+e[0].values[1]+`</td>
+                  </tr>`
+        }
       }
+        
     })
     .catch(err =>{
         console.log(err);
     })
     document.querySelector(".table").innerHTML = tbheader;
-    document.getElementById('tbody').innerHTML=html;
+    document.getElementById('tbody').innerHTML = html;
   }
 
   async function drawTableLight() {
     let html = "";
-    let arr = [];
     let tbheader='';
     await fetch("/apicall/light")
     .then((res)=> res.json())
@@ -53,23 +56,26 @@ async function drawTableTH() {
       for(let i = dat.length -1;i>0;i--){
         e = dat[i];
         dtime =  dateTimeConvert(e.time_stamp).toString();
-        arr.push([dtime,e[0].values[0]]);
-        let tdstyle = "";
-        (e[0].values[0]>60||e[0].values[0]<15)?tdstyle = "color: red":"";
-        tbheader = `<thead>
-                        <tr>
-                            <th>Time</th>
-                            <th>SensorID</th>
-                            <th>Light</th>
-                        </tr>
-                    </thead>
-                    <tbody id="tbody">
-                        
-                    </tbody>`
-        html += `<tr><td>`+dtime+`</td>
-                    <td>`+e[0].device_id+`</td>
-                    <td style = '`+tdstyle+`'>`+e[0].values[0]+`</td>
-                </tr>`
+        var ts =  dateTimeConvert(e.time_stamp);
+        var timeFormat = ts.getFullYear() + "-" + ((ts.getMonth()+1)<10 ? '0' + (ts.getMonth()+1) : ts.getMonth()+1) + "-" + ts.getDate();
+        if(timeFormat==document.getElementById("date-input").value || document.getElementById("date-input").value==""){
+          let tdstyle = "";
+          (e[0].values[0]>60||e[0].values[0]<15)?tdstyle = "color: red":"";
+          tbheader = `<thead>
+                          <tr>
+                              <th>Time</th>
+                              <th>SensorID</th>
+                              <th>Light</th>
+                          </tr>
+                      </thead>
+                      <tbody id="tbody">
+                          
+                      </tbody>`
+          html += `<tr><td>`+dtime+`</td>
+                      <td>`+e[0].device_id+`</td>
+                      <td style = '`+tdstyle+`'>`+e[0].values[0]+`</td>
+                  </tr>`
+        }
       }
     })
     .catch(err =>{
@@ -77,25 +83,4 @@ async function drawTableTH() {
     })
     document.querySelector(".table").innerHTML = tbheader;
     document.getElementById('tbody').innerHTML=html;
-}
-
-function drawChart() {
-    var data = google.visualization.arrayToDataTable([
-      ['Tháng', 'Nhiệt độ', 'Expenses'],
-      ['Tháng 1',  1000,      400],
-      ['Tháng 2',  1170,      460],
-      ['Tháng 3',  660,       1120],
-      ['Tháng 4',  1030,      540]
-    ]);
-
-    var options = {
-      title: 'Biểu đồ các chỉ số của trang trại',
-      curveType: 'function',
-      legend: { position: 'bottom' },
-      width: '500px',
-      height: '300px'
-    };
-
-    var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
-    chart.draw(data, options);
 }
