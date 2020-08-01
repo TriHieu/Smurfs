813cfdde3a9ac445626c9763d0c16d3a07791253
Hoàng Trương <hoangtruong181099@gmail.com>
2020-08-01 21:27:34 +0700

    final js

--- App/js/dashboard1.js
+++ App/js/dashboard1.js
@@ -3,13 +3,136 @@ function dateTimeConvert(timestamp){
     return date;
 }
 
-async function callAPI() {
-    await fetch("/apicall/temphumi/latest")
+var slider = document.getElementById("myRange");
+var output = document.getElementById("Speaker");
+var inpnum = document.getElementById("inp_number");
+output.innerHTML = slider.value;
+
+slider.oninput = function () {
+    output.innerHTML = this.value;
+    inpnum.value = this.value;
+}
+inpnum.oninput = function () {
+    output.innerHTML = this.value;
+    slider.value = this.value;
+}
+async function fun(value) {
+    if (parseInt(value) >= 5000) { value = "5000"; }
+    if (parseInt(value) == 0){
+        values = ["0","0"];
+    }
+    else{
+        values = ["1",value];
+    }
+    await fetch("apicall/publishSpeaker", {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json'
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify([{ "device_id": "Speaker", "values": values }]) // body data type must match "Content-Type" header
+    });
+
+}
+
+async function getData(action) {
+    uid = document.getElementById('hid').value;
+    await fetch("users/"+uid)
+        .then(res=>res.json())
+        .then(sensors=>{
+            switch(action){
+                case 'drawChartTH':
+                    drawChartTH(sensors);
+                    break;
+                case 'drawChartLight':
+                    drawChartLight(sensors);
+                    break;
+                case 'callAPI':
+                    callAPI(sensors);
+                    break;
+            }
+
+    })   
+}
+
+async function callAPI(sensors){
+    sensorsTH = [];
+    sensorsLight=[];
+    try{ 
+        sensorsTH = sensors[0]['TempHumi'].devices.device_id;
+        sensorsLight = sensors[0]['Light'].devices.device_id;
+    }
+    catch(e){}
+
+    await fetch("apicall/getspeaker")
         .then((res) => res.json())
         .then(data => {
-            //console.log(data[0].values[0]); // JSON object
-            document.getElementById('Temperature').innerHTML = data[0].values[0] + " C";
-            document.getElementById('Humid').innerHTML = data[0].values[1] + " %";
+            //console.log("Speaker", data[0].values[1]); // JSON object
+            status = data[0].values[0];
+            speak = data[0].values[1];
+            document.getElementById('Speaker').innerHTML = speak;
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    val = document.getElementById("myRange").value;
+    
+
+    if((sensorsTH.length==0&&sensorsLight.length==0)){
+        document.getElementById('data-display').innerHTML= "Nothing to Display. Go Register some Sensors !!"
+        return;
+    }
+    else{
+        await fetch("/apicall/temphumi/latest")
+        .then((res) => res.json())
+        .then(data => {
+            let div = '';
+            for (e in data){
+                try{
+                    if(data[e][0]["device_id"]==sensorsTH.find(i=>i==data[e][0]["device_id"])){
+                        let tdstyle0 = "";
+                        let tdstyle1 = "";
+                        (data[e][0].values[0]>28||data[e][0].values[0]<22)?tdstyle0 = "color: red":"";
+                        (data[e][0].values[1]>75||data[e][0].values[1]<70)?tdstyle1 = "color: red":"";
+
+                        div += `<div class="card"">
+                                    <div class="card-body" style="width:120px;">
+                                    <p>DeviceID: </p>
+                                    <label>`+data[e][0]['device_id']+`</label>
+                                    <p>Temperature: </p>
+                                    <label style = '`+tdstyle0+`'>`+data[e][0].values[0]+`</label>
+                                    <p>Humid: </p>
+                                    <label style = '`+tdstyle1+`'>`+data[e][0].values[1]+`</label>
+                                    </div>
+                                </div>`
+                        // console.log(data[e][0]['device_id']); // JSON object
+                        if (parseInt(speak) <= 1000 && parseInt(data[e][0].values[0]) >= 40 && parseInt(status)!=0) {
+                            console.log('running')
+                            fetch("apicall/publishSpeaker", {
+                                method: 'POST', // *GET, POST, PUT, DELETE, etc.
+                                mode: 'cors', // no-cors, *cors, same-origin
+                                cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+                                credentials: 'same-origin', // include, *same-origin, omit
+                                headers: {
+                                    'Content-Type': 'application/json'
+                                    // 'Content-Type': 'application/x-www-form-urlencoded',
+                                },
+                                redirect: 'follow', // manual, *follow, error
+                                referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+                                body: JSON.stringify([{ "device_id": "Speaker", "values": ["1", "3000"] }]) // body data type must match "Content-Type" header
+                            });
+                        }
+                    }
+                }
+                catch(err){
+                }
+            }
+            document.getElementById('th').innerHTML = div;
         })
         .catch(err => {
             console.log(err);
@@ -18,16 +141,67 @@ async function callAPI() {
     await fetch("/apicall/light/latest")
         .then((res) => res.json())
         .then(data => {
-            //light = data[0].values;
-            document.getElementById('Light').innerHTML = data[0].values + " lux";
+            let div = '';
+            for (e in data){
+                try{
+                    if(data[e][0]["device_id"]==sensorsLight.find(i=>i==data[e][0]["device_id"])){
+                        let tdstyle = "";
+                        (data[e][0].values[0]>60||data[e][0].values[0]<15)?tdstyle = "color: red":"";
+                        div += `<div class="card">
+                                    <div class="card-body" style="width:120px;">
+                                    <p>DeviceID: </p>
+                                    <label>`+data[e][0]['device_id']+`</label>
+                                    <p>Light: </p>
+                                    <label style = '`+tdstyle+`'>`+data[e][0].values+`</label>
+                                    </div>
+                                </div>`
+                    }
+                }
+                catch(err){
+                }
+            }
+            document.getElementById('lg').innerHTML = div;
         })
         .catch(err => {
             console.log(err);
         })
+    }
+    
 }
 // }
 
-async function drawChartLight(){
+async function drawChartLight(sensors){
+    let notes = `
+                <li>
+                    <h5><i class="fa fa-circle m-r-5 text-inverse"></i>Light</h5> </li>
+                `
+    document.getElementById('notes').innerHTML=notes;
+    sensorsLight=[];
+    try{ 
+        sensorsLight = sensors[0]['Light'].devices.device_id;
+    }
+    catch(e){}
+
+    if(sensorsLight.length==0){
+        new Chartist.Line('#ct-visits', {
+            labels: '',
+            series: [[]]
+        }, {
+            top: 0,
+            low: 1,
+            showPoint: true,
+            fullWidth: true,
+            plugins: [Chartist.plugins.tooltip()],
+            axisY: {
+                labelInterpolationFnc: function (value) {
+                    return (value / 1);
+                }
+            },
+            showArea: true
+        });
+        return;
+    }
+
     let arr=[];
     await fetch("/apicall/light")
     .then((res)=> res.json())
@@ -40,18 +214,12 @@ async function drawChartLight(){
     .catch(err =>{
         console.log(err);
     })
-    let notes = `
-                <li>
-                    <h5><i class="fa fa-circle m-r-5 text-inverse"></i>Light</h5> </li>
-                `
-    document.getElementById('notes').innerHTML=notes;
     let time =[];
     let light = [];
     for(let i=0;i<8;i++){
         time[i]= arr[8-i][0];
         light[i] = arr[8-i][1];
     }
-    console.log(light);
     //ct-visits
     new Chartist.Line('#ct-visits', {
         labels: time,
@@ -71,34 +239,68 @@ async function drawChartLight(){
     });
 }
 
-async function drawChartTH(){
-    let arr=[];
-    await fetch("/apicall/temphumi")
-    .then((res)=> res.json())
-    .then(dat => {
-        for(let i = dat.length -1;i>0;i--){
-            if (dat[i][0] == undefined)
-                continue;
-            dtime =  dateTimeConvert(dat[i].time_stamp).toLocaleTimeString();
-            arr.push([dtime,dat[i][0].values[0],dat[i][0].values[1]]);
-        }
-    })
-    .catch(err =>{
-        console.log(err);
-    })
+async function drawChartTH(sensors){
     let notes = `<li>
                     <h5><i class="fa fa-circle m-r-5 text-info"></i>Humid</h5> </li>
                 <li>
                     <h5><i class="fa fa-circle m-r-5 text-inverse"></i>Temperature</h5> </li>
                 `
     document.getElementById('notes').innerHTML=notes;
+
+    sensorsTH = [];
+    try{ 
+        sensorsTH = sensors[0]['TempHumi'].devices.device_id;
+    }
+    catch(e){}
+    console.log(sensorsTH);
+    if(sensorsTH.length==0){
+        new Chartist.Line('#ct-visits', {
+            labels: "",
+            series: [[],[]]
+        }, {
+            top: 0,
+            low: 1,
+            showPoint: true,
+            fullWidth: true,
+            plugins: [Chartist.plugins.tooltip()],
+            axisY: {
+                labelInterpolationFnc: function (value) {
+                    return (value / 1);
+                }
+            },
+            showArea: true
+        });
+        return;
+    }
+
+    let arr=[];
+    await fetch("/apicall/temphumi")
+    .then((res)=> res.json())
+    .then(dat => {
+        // console.log(sensorsTH);
+        for(let i = dat.length -1;i>1;i--){
+            if (dat[i][0] == undefined)
+                continue;
+            if(dat[i][0]["device_id"]==sensorsTH.find(e=>e==dat[i][0]["device_id"])){
+                dtime =  dateTimeConvert(dat[i].time_stamp).toLocaleTimeString();
+                arr.push([dtime,dat[i][0].values[0],dat[i][0].values[1]]);
+            }
+        }
+    })
+    .catch(err =>{
+        console.log(err);
+    })
     let time =[];
     let temp = [];
-    let humid = []
-    for(let i=0;i<8;i++){
-        time[i]= arr[8-i][0];
-        temp[i] = arr[8-i][1];
-        humid[i] = arr[8-i][2];
+    let humid = [];
+    count = 0;
+    l = arr.length;
+    for(let i=0;i<arr.length-1;i++){
+        time[i]= arr[l-i-1][0];
+        temp[i] = arr[l-i-1][1];
+        humid[i] = arr[l-i-1][2];
+        count++;
+        if (count == 8) break;
     }
     //ct-visits
     new Chartist.Line('#ct-visits', {
@@ -119,14 +321,14 @@ async function drawChartTH(){
     });
 }
 
+
+setTimeout(()=>getData('callAPI'), 100);
+setInterval(()=>getData('callAPI'),10000);
+setTimeout(()=>getData('drawChartTH'),100);
+
 $(document).ready(function () {
      "use strict";
      // toat popup js
-    drawChartTH();
-    callAPI();
-    setInterval(callAPI,10000);
-    
-    
      // counter
      $(".counter").counterUp({
          delay: 100,
--- App/js/history.js
+++ App/js/history.js
@@ -3,9 +3,40 @@ function dateTimeConvert(timestamp){
   return date;
 }
 
-async function drawTableTH() {
+async function getData(action) {
+  uid = document.getElementById('hid').value;
+  await fetch("users/"+uid)
+      .then(res=>res.json())
+      .then(sensors=>{
+          switch(action){
+              case 'drawTableTH':
+                  drawTableTH(sensors);
+                  break;
+              case 'drawTableLight':
+                  drawTableLight(sensors);
+                  break;
+          }
+  })   
+}
+
+async function drawTableTH(sensors) {
+    sensorsTH = [];
+    try{ 
+        sensorsTH = sensors[0]['TempHumi'].devices.device_id;
+    }catch(er){}
+    console.log(sensorsTH);
+
     let html = "";
-    let tbheader='';
+    let tbheader = `<thead>
+                                <tr>
+                                    <th>Time</th>
+                                    <th>DeviceID</th>
+                                    <th>Temperature (C)</th>
+                                    <th>Humidity (%)</th>
+                                </tr>
+                            </thead>
+                            <tbody id="tbody">
+                            </tbody>`;
     await fetch("/apicall/temphumi")
     .then((res)=> res.json())
     .then(dat => {
@@ -13,29 +44,22 @@ async function drawTableTH() {
         var e = dat[i];
         if (e[0]==undefined) // check error
           continue; //skip
-        dtime =  dateTimeConvert(e.time_stamp).toString();
-        var ts =  dateTimeConvert(e.time_stamp);
-        var t = ts.getFullYear()+"-"+((ts.getMonth()+1)<10?'0' + (ts.getMonth()+1):ts.getMonth()+1)+"-"+ts.getDate();
-        let tdstyle0 = "";
-        let tdstyle1 = "";
-        if(t==document.getElementById("date-input").value || document.getElementById("date-input").value==""){
-            (e[0].values[0]>26||e[0].values[0]<22)?tdstyle0 = "color: red":"";
-            (e[0].values[1]>75||e[0].values[1]<70)?tdstyle1 = "color: red":"";
-          tbheader = `<thead>
-                          <tr>
-                              <th>Time</th>
-                              <th>SensorID</th>
-                              <th>Temperature (C)</th>
-                              <th>Humidity (%)</th>
-                          </tr>
-                      </thead>
-                      <tbody id="tbody">
-                      </tbody>`
-          html += `<tr><td>`+dtime+`</td>
-                      <td>`+e[0].device_id+`</td>
-                      <td style = '`+tdstyle0+`'>`+e[0].values[0]+`</td>
-                      <td style = '`+tdstyle1+`'>`+e[0].values[1]+`</td>
-                  </tr>`
+        if(e[0]["device_id"]==sensorsTH.find(u=>u==e[0]["device_id"])){
+          dtime =  dateTimeConvert(e.time_stamp).toString();
+          var ts =  dateTimeConvert(e.time_stamp);
+          var t = ts.getFullYear()+"-"+((ts.getMonth()+1)<10?'0' + (ts.getMonth()+1):ts.getMonth()+1)+"-"+ts.getDate();
+          let tdstyle0 = "";
+          let tdstyle1 = "";
+          if(t==document.getElementById("date-input").value || document.getElementById("date-input").value==""){
+              (e[0].values[0]>26||e[0].values[0]<22)?tdstyle0 = "color: red":"";
+              (e[0].values[1]>75||e[0].values[1]<70)?tdstyle1 = "color: red":"";
+            
+            html += `<tr><td>`+dtime+`</td>
+                        <td>`+e[0].device_id+`</td>
+                        <td style = '`+tdstyle0+`'>`+e[0].values[0]+`</td>
+                        <td style = '`+tdstyle1+`'>`+e[0].values[1]+`</td>
+                    </tr>`
+          }
         }
       }
         
@@ -47,34 +71,45 @@ async function drawTableTH() {
     document.getElementById('tbody').innerHTML = html;
   }
 
-  async function drawTableLight() {
+  async function drawTableLight(sensors) {
+    
+    sensorsLight = [];
+    try{ 
+        sensorsLight = sensors[0]['Light'].devices.device_id;
+    }catch(er){}
+    console.log(sensorsLight);
+
     let html = "";
-    let tbheader='';
+    let tbheader = `<thead>
+                        <tr>
+                            <th>Time</th>
+                            <th>DeviceID</th>
+                            <th>Light</th>
+                        </tr>
+                    </thead>
+                    <tbody id="tbody">
+                        
+                    </tbody>`
+
     await fetch("/apicall/light")
     .then((res)=> res.json())
     .then(dat => {
       for(let i = dat.length -1;i>0;i--){
         e = dat[i];
-        dtime =  dateTimeConvert(e.time_stamp).toString();
-        var ts =  dateTimeConvert(e.time_stamp);
-        var timeFormat = ts.getFullYear() + "-" + ((ts.getMonth()+1)<10 ? '0' + (ts.getMonth()+1) : ts.getMonth()+1) + "-" + ts.getDate();
-        if(timeFormat==document.getElementById("date-input").value || document.getElementById("date-input").value==""){
-          let tdstyle = "";
-          (e[0].values[0]>60||e[0].values[0]<15)?tdstyle = "color: red":"";
-          tbheader = `<thead>
-                          <tr>
-                              <th>Time</th>
-                              <th>SensorID</th>
-                              <th>Light</th>
-                          </tr>
-                      </thead>
-                      <tbody id="tbody">
-                          
-                      </tbody>`
-          html += `<tr><td>`+dtime+`</td>
-                      <td>`+e[0].device_id+`</td>
-                      <td style = '`+tdstyle+`'>`+e[0].values[0]+`</td>
-                  </tr>`
+        if (e[0]==undefined) // check error
+          continue; //skip
+        if(e[0]["device_id"]==sensorsLight.find(u=>u==e[0]["device_id"])){
+          dtime =  dateTimeConvert(e.time_stamp).toString();
+          var ts =  dateTimeConvert(e.time_stamp);
+          var timeFormat = ts.getFullYear() + "-" + ((ts.getMonth()+1)<10 ? '0' + (ts.getMonth()+1) : ts.getMonth()+1) + "-" + ts.getDate();
+          if(timeFormat==document.getElementById("date-input").value || document.getElementById("date-input").value==""){
+            let tdstyle = "";
+            (e[0].values[0]>60||e[0].values[0]<15)?tdstyle = "color: red":"";
+            html += `<tr><td>`+dtime+`</td>
+                        <td>`+e[0].device_id+`</td>
+                        <td style = '`+tdstyle+`'>`+e[0].values[0]+`</td>
+                    </tr>`
+          }
         }
       }
     })
@@ -84,3 +119,6 @@ async function drawTableTH() {
     document.querySelector(".table").innerHTML = tbheader;
     document.getElementById('tbody').innerHTML=html;
 }
+
+
+setTimeout(()=>getData('drawTableTH'),100)
